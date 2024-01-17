f2abf6ecb4b43433a9734b228ebd3827bd2b6299
KhezamiTaha <khezamitaha10@gmail.com>
2024-01-17 15:17:00 +0000

    0-binary_tree_node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_node- Algorithms function
+ *@parent: pointer
+ *@value: pointer
+ *
+ *Return: 1 or 0
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	
+}
+
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
